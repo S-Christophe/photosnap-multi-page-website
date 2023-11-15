174fb20510b8e6b6fa2935790a844a37a5c537ad
S-Christophe <christopheshao@gmail.com>
2023-11-15 14:10:46 +0100

    Header: add responsive desktop

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -13,7 +13,10 @@ export default function Header() {
 
   return (
     <header className="flex flex-row items-center bg-white">
-      <Link href="/" className="my-7 ml-6 md:ml-[2.46875rem]">
+      <Link
+        href="/"
+        className="my-7 ml-6 md:ml-[2.46875rem] lg:ml-[10.3125rem]"
+      >
         <Image
           src="./assets/images/shared/desktop/logo.svg"
           alt="Photosnap"
@@ -25,19 +28,19 @@ export default function Header() {
         initial="hidden"
         animate={isActive ? "visible" : "hidden"}
         variants={animateMenu}
-        className="absolute top-[4.5rem] w-full bg-white px-[2.03125rem] py-8 md:static md:ml-auto md:mr-[2.46875rem] md:!block md:w-auto md:px-0 md:py-0 md:!opacity-100"
+        className="absolute top-[4.5rem] w-full bg-white px-[2.03125rem] py-8 md:static md:ml-auto md:mr-[2.46875rem] md:!block md:w-auto md:px-0 md:py-0 md:!opacity-100 lg:mr-[10.3125rem] lg:flex-grow"
       >
-        <ul className="flex flex-col gap-5 text-center font-DmSansBold uppercase tracking-[0.15625rem] md:flex-row md:items-center md:gap-[2.3125rem] md:text-[0.75rem] md:tracking-[0.125rem]">
-          <li>
+        <ul className="flex flex-col gap-5 text-center font-DmSansBold uppercase tracking-[0.15625rem] md:flex-row md:items-center md:gap-[2.3125rem] md:text-[0.75rem] md:tracking-[0.125rem] lg:gap-0">
+          <li className="lg:ml-auto">
             <Link href="#">Stories</Link>
           </li>
-          <li>
+          <li className="lg:ml-[2.3125rem]">
             <Link href="#">Features</Link>
           </li>
-          <li>
+          <li className="lg:ml-[2.3125rem]">
             <Link href="#">Pricing</Link>
           </li>
-          <li className="relative mt-5 before:absolute before:-top-5 before:left-0 before:w-full before:border md:ml-[0.875rem] md:mt-0 md:before:hidden">
+          <li className="relative mt-5 before:absolute before:-top-5 before:left-0 before:w-full before:border md:ml-[0.875rem] md:mt-0 md:before:hidden lg:ml-auto">
             <Link
               href="#"
               className="block w-full bg-black py-[0.875rem] text-white md:px-6 md:py-3"
