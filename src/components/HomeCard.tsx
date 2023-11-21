0aedfca155e142a5501d9f4ba9150b8a9842f081
S-Christophe <christopheshao@gmail.com>
2023-11-21 10:34:39 +0100

    Fix svg fillRule

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -58,7 +58,7 @@ export default function HomeCard({
           <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
             <g
               fill="none"
-              fill-rule="evenodd"
+              fillRule="evenodd"
               stroke={`${index == 0 ? "#fff" : "#000"}`}
             >
               <path d="M0 7h41.864M35.428 1l6 6-6 6" />
