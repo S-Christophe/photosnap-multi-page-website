e1480781b162b5380196fbce8a78d7ac256c4f11
S-Christophe <christopheshao@gmail.com>
2023-11-21 10:07:04 +0100

    Add z index to nav mobile

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
         initial="hidden"
         animate={isActive ? "visible" : "hidden"}
         variants={animateMenu}
-        className="absolute top-[4.5rem] w-full bg-white px-[2.03125rem] py-8 md:static md:mr-[2.46875rem] md:!block md:w-auto md:flex-grow md:px-0 md:py-0 md:!opacity-100 lg:mr-[10.3125rem]"
+        className="absolute top-[4.5rem] z-10 w-full bg-white px-[2.03125rem] py-8 md:static md:mr-[2.46875rem] md:!block md:w-auto md:flex-grow md:px-0 md:py-0 md:!opacity-100 lg:mr-[10.3125rem]"
       >
         <ul className="flex flex-col gap-5 text-center font-DmSansBold uppercase tracking-[0.15625rem] md:flex-row md:items-center md:gap-0 md:text-[0.75rem] md:tracking-[0.125rem]">
           <li className="md:ml-auto">
