7021110caea729d73dcd83aa065f4cff5d8494e9
S-Christophe <christopheshao@gmail.com>
2023-12-13 22:38:12 +0100

    Create footer (mobile)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 // import components
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
